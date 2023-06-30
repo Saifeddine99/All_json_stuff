1eac78348f8eda64d759d02927a0c9504205e702
Saifeddine Baccouche <saif.bac1999@gmail.com>
2023-06-30 12:08:51 +0100

    fixing center bottom issue

--- Json_files_treat_recommendation/no_clinical_conditions/treatment_reco_main.py
+++ Json_files_treat_recommendation/no_clinical_conditions/treatment_reco_main.py
@@ -9,10 +9,20 @@ def find_item_1(med_dose_last_time):
             item=key
     return(item)
 
+def niad_check(med_dose_last_time):
+    niad_list=['DPP4i', 'oral GLP1ra', 'SGLT2i', 'Pio', 'SU']
+    score=0
+    for key in med_dose_last_time:
+        if key in niad_list :
+            score+=1
+    if ('DPP4i' in med_dose_last_time and 'oral GLP1ra' in med_dose_last_time):
+        score=0
+    return(score)
+
 def recommended_treatment(med_dose_last_time,hba1c_records,target,symptoms):
 
     #this is the list of the second/third level medications
-    second_or_third_med_level=['DPP4i', 'SGLT2i', 'oral GLP1ra', 'Pio', 'SU']
+    niad_list=['DPP4i', 'SGLT2i', 'oral GLP1ra', 'Pio', 'SU']
 
     current_hba1c=hba1c_records[0]
     previous_hba1c=hba1c_records[1]
@@ -53,47 +63,58 @@ def recommended_treatment(med_dose_last_time,hba1c_records,target,symptoms):
                 proposed_med["Metformin"]=full_dose
             elif (8<current_hba1c<=9):
                 proposed_med["Metformin"]=full_dose
-                proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=medium_dose
+                proposed_med["You can choose any item from this list: {}".format(niad_list)]=medium_dose
             else:
                 proposed_med={}
                 proposed_med["Visit a doctor"]="Critical situation"
         else:
-            proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=medium_dose
+            proposed_med["You can choose any item from this list: {}".format(niad_list)]=medium_dose
     # Here we're working on Metformin + only one drug from the list
-    elif ((len(med_dose_last_time)==2 and not("nonpharmacological therapy" in med_dose_last_time) and not("Basal insulin" in med_dose_last_time) and ("Metformin" in med_dose_last_time)) or (("nonpharmacological therapy" in med_dose_last_time) and not("Basal insulin" in med_dose_last_time) and len(med_dose_last_time)==3 and ("Metformin" in med_dose_last_time))):
+    elif ((len(med_dose_last_time)==2 and not("nonpharmacological therapy" in med_dose_last_time)) or (("nonpharmacological therapy" in med_dose_last_time) and len(med_dose_last_time)==3)) and niad_check(med_dose_last_time)==1 and not("Basal insulin" in med_dose_last_time) and ("Metformin" in med_dose_last_time):
         if(symptoms=="YES" and current_hba1c>9 and not("nonpharmacological therapy" in med_dose_last_time)):
             proposed_med={} 
             proposed_med["Metformin"]=full_dose
             proposed_med["Basal insulin"]=full_dose
-        else:    
+        else:  
             first_med_from_list= find_item_1(med_dose_last_time)
+
+            if(first_med_from_list=="DPP4i"):
+                niad_list.remove("oral GLP1ra")
+            elif(first_med_from_list=="oral GLP1ra"):
+                niad_list.remove("DPP4i")
+
             if(target<current_hba1c<=8):
                 if(med_dose_last_time["Metformin"]==med_dose_last_time[first_med_from_list]==full_dose):
-                    second_or_third_med_level.remove(first_med_from_list)
-                    proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=full_dose
+                    niad_list.remove(first_med_from_list)
+                    proposed_med["You can choose any item from this list: {}".format(niad_list)]=full_dose
                 else:
                     proposed_med["Metformin"]=full_dose
                     proposed_med[first_med_from_list]=full_dose    
             elif (8<current_hba1c<=9):
                 proposed_med["Metformin"]=full_dose
                 proposed_med[first_med_from_list]=full_dose 
-                second_or_third_med_level.remove(first_med_from_list)
-                proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=full_dose
+                niad_list.remove(first_med_from_list)
+                proposed_med["You can choose any item from this list: {}".format(niad_list)]=full_dose
             else:
                 proposed_med={}
                 proposed_med["Visit a doctor"]="Critical situation"
     # Here we're working on Metformin + strictly more than 1 drug from the list
-    elif ((len(med_dose_last_time)>=3 and not("nonpharmacological therapy" in med_dose_last_time) and not("Basal insulin" in med_dose_last_time) and ("Metformin" in med_dose_last_time)) or (("nonpharmacological therapy" in med_dose_last_time) and not("Basal insulin" in med_dose_last_time) and len(med_dose_last_time)>=4 and ("Metformin" in med_dose_last_time))):
-    
+    elif ((len(med_dose_last_time)>=3 and not("nonpharmacological therapy" in med_dose_last_time)) or (("nonpharmacological therapy" in med_dose_last_time) and len(med_dose_last_time)>=4)) and not("Basal insulin" in med_dose_last_time) and ("Metformin" in med_dose_last_time) and niad_check(med_dose_last_time)>=2:
+        
+        if("DPP4i" in med_dose_last_time):
+            niad_list.remove("oral GLP1ra")
+        elif("oral GLP1ra" in med_dose_last_time):
+            niad_list.remove("DPP4i")
+
         if(target<current_hba1c<=8):
                 #Here we will add another drug
                 for key in med_dose_last_time:
                     if(key!= "nonpharmacological therapy"):
                         proposed_med[key]=full_dose
                     if(key != "nonpharmacological therapy" and key != "Metformin"):
-                        second_or_third_med_level.remove(key)
-                if( len(second_or_third_med_level)>0):
-                    proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=full_dose  
+                        niad_list.remove(key)
+                if( len(niad_list)>0):
+                    proposed_med["You can choose any item from this list: {}".format(niad_list)]=full_dose  
                 else:
                     proposed_med={}
                     proposed_med["Basal insulin"]=full_dose
@@ -108,7 +129,7 @@ def recommended_treatment(med_dose_last_time,hba1c_records,target,symptoms):
         proposed_med={}
         proposed_med["Metformin"]=full_dose
         if(symptoms=="NO" and current_hba1c<=9):
-            proposed_med["You can choose any item from this list: {}".format(second_or_third_med_level)]=full_dose        
+            proposed_med["You can choose any item from this list: {}".format(niad_list)]=full_dose        
         else:
             proposed_med["Basal insulin"]=full_dose
     elif("Basal insulin" in med_dose_last_time):
