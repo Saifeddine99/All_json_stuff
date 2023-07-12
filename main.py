acd64c2a133375d2d245afe63e96d34cd275fc5e
Saifeddine Baccouche <saif.bac1999@gmail.com>
2023-07-12 10:19:47 +0100

    adding lottie icons

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from json_learning.get_patient_data import patient_EHRs
 from Json_files_treat_recommendation.main_treat_recom import main_get_treat
+import requests
+from streamlit_lottie import st_lottie
+
 
 st.set_page_config(page_title="Json stuff", page_icon=":hospital:", layout="centered")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 selected=option_menu(
     menu_title="Main Menu",
@@ -25,21 +34,34 @@ selected=option_menu(
             },
 )
 
+lottie_create_json = load_lottieurl("https://lottie.host/1c02cdb3-669b-405d-a2a0-bc60da003400/k757wwytXM.json")
+lottie_treatment = load_lottieurl("https://lottie.host/19713cf4-b810-4c41-ac44-a3206e833307/hWghUpQBeJ.json")
+
 if selected=="Home":
     st.write('#')
     st.write('#')
     st.markdown(
-    "<h1 style=' color: #008000;'>Hello! You are in the home page:  </h1>", 
+    "<h1 style=' color: #0B5345;'>Hello! You are in the home page:  </h1>", 
     unsafe_allow_html = True
     )
-    st.markdown(
-    "<h1 style='color: #000d1a;'>1/Click on 'Create json' to create a Json file in the OpenEHR standards format.  </h1>", 
-    unsafe_allow_html = True
-    )
-    st.markdown(
-    "<h1 style='color: #000d1a;'>2/Click on 'Get treatment' to enter your EHRs's json file and get treatment.  </h1>", 
-    unsafe_allow_html = True
-    )
+    text, anim = st.columns((2, 1))
+    with text:
+        st.write('#')
+        st.subheader("1/Click on 'Create json' to create a Json file in the OpenEHR standards format")
+    with anim:
+        st_lottie(lottie_create_json, height=200, key="json_creation")
+
+    st.write('#')
+    st.write('#')
+
+    with text:
+        st.write('#')
+        st.write('#')
+        st.write('#')
+        st.subheader("2/Click on 'Get treatment' to enter your EHRs's json file and get treatment")
+    with anim:
+        st_lottie(lottie_treatment, height=200, key="teatment_recommendation")
+
 
 if selected=="Create json":
     patient_EHRs()
